c59bf7c04edff57e9fbbcc6f5ad08b63dfd8039a
KangBaekGwa <ksu9801@gmail.com>
2024-11-12 18:51:30 +0900

    Feat : Binding Error 공통 응답 코드 작성 및 HttpStatus 응답 안되도록 Ignore 설정

--- src/main/java/baekgwa/auctionservice/global/common/response/BaseResponseCode.java
+++ src/main/java/baekgwa/auctionservice/global/common/response/BaseResponseCode.java
@@ -12,7 +12,11 @@ public enum BaseResponseCode {
 
     //1000 ~ 2000
     //오류 종류 : 회원 서비스 오류
+    FIND_USER_ERROR_NOT_FIND(HttpStatus.BAD_REQUEST, Boolean.FALSE, 1000, "잘못된 회원 정보"),
 
+    //9000 ~ 9999
+    //오류 종류 : 공통 에러
+    VALIDATION_FAIL_ERROR(HttpStatus.BAD_REQUEST, Boolean.FALSE, 9000, "(exception error 메세지에 따름)"),
     FAIL(HttpStatus.BAD_REQUEST, Boolean.FALSE, 400, "요청 응답 실패");
 
     private final HttpStatus httpStatus;
