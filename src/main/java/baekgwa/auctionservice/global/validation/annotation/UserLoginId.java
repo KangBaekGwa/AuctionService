96c2bc580688851255da82925ac2469e8405b477
KangBaekGwa <ksu9801@gmail.com>
2024-11-12 18:50:14 +0900

    Chore : 쓸모없는 유효성 검증 삭제

--- src/main/java/baekgwa/auctionservice/global/validation/annotation/UserLoginId.java
+++ src/main/java/baekgwa/auctionservice/global/validation/annotation/UserLoginId.java
@@ -13,7 +13,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Constraint(validatedBy = { })
 @Retention(RetentionPolicy.RUNTIME)
-@NotBlank(message = "로그인 아이디는 필수값 입니다.")
 @Size(min = 5, max = 20, message = "로그인 아이디는 5자리 ~ 20자리 사이입니다.")
 @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "로그인 아이디는 영문(대소문자 구분)과 숫자만 허용합니다.")
 public @interface UserLoginId {
