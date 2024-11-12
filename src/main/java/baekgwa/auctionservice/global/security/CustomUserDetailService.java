3f2929828f716d88f172665340e9da03537c271d
KangBaekGwa <ksu9801@gmail.com>
2024-11-12 16:53:06 +0900

    Feat : Security 설정 진행

---
+++ src/main/java/baekgwa/auctionservice/global/security/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package baekgwa.auctionservice.global.security;
+
+import baekgwa.auctionservice.domain.user.entity.User;
+import baekgwa.auctionservice.domain.user.repository.UserRepository;
+import baekgwa.auctionservice.global.common.exception.CustomException;
+import baekgwa.auctionservice.global.common.response.BaseResponseCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userData = userRepository.findByLoginId(username).orElseThrow(
+                () -> new CustomException(BaseResponseCode.FIND_USER_ERROR_NOT_FIND)
+        );
+
+        return new CustomUserDetails(userData);
+    }
+}
