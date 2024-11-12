74fe4c90771b3240ea32bedaa02711543613e30e
KangBaekGwa <ksu9801@gmail.com>
2024-11-12 20:26:45 +0900

    Feat : User 도메인 Repository 레이어 기능 구현

---
+++ src/main/java/baekgwa/auctionservice/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package baekgwa.auctionservice.domain.user.repository;
+
+import baekgwa.auctionservice.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLoginId(String loginId);
+}
